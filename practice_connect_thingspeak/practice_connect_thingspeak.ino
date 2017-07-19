8f332568c8ef09967062efbd2589e684c7bc4c87
20123362 <eodhs@naver.com>
2017-07-18 17:00:43 -0700

    version1 commit

---
+++ practice_connect_thingspeak/practice_connect_thingspeak.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+// Include the SparkFun Phant library.
+#include <Phant.h>
+#include <Wire.h>
+#include "MAX30100.h"
+
+// Tweakable parameters
+// Sampling and polling frequency must be set consistently
+#define POLL_PERIOD_US                      1E06 / 100
+#define SAMPLING_RATE                       MAX30100_SAMPRATE_100HZ
+
+// The LEDs currents must be set to a level that avoids clipping and maximises the
+// dynamic range
+#define IR_LED_CURRENT                      MAX30100_LED_CURR_50MA
+#define RED_LED_CURRENT                     MAX30100_LED_CURR_27_1MA
+
+// The pulse width of the LEDs driving determines the resolution of
+// the ADC (which is a Sigma-Delta).
+// set HIGHRES_MODE to true only when setting PULSE_WIDTH to MAX30100_SPC_PW_1600US_16BITS
+#define PULSE_WIDTH                         MAX30100_SPC_PW_1600US_16BITS
+#define HIGHRES_MODE                        true
+
+
+// Instantiate a MAX30100 sensor class
+MAX30100 sensor;
+uint32_t tsLastPollUs = 0;
+
+String apiKey = "50SAMLSII2Y9O521";
+const char* ssid = "UCInet Mobile Access";
+const char* password = "";
+  float sensor_data=0;
+
+const char* server = "api.thingspeak.com";
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+
+    Serial.print("Initializing MAX30100..");
+
+    // Initialize the sensor
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!sensor.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+    // Set up the wanted parameters
+    sensor.setMode(MAX30100_MODE_SPO2_HR);
+    sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
+    sensor.setLedsPulseWidth(PULSE_WIDTH);
+    sensor.setSamplingRate(SAMPLING_RATE);
+    sensor.setHighresModeEnabled(HIGHRES_MODE);
+  
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  if (micros() < tsLastPollUs || micros() - tsLastPollUs > POLL_PERIOD_US) {
+        sensor.update();
+        tsLastPollUs = micros();   
+        sensor_data = sensor.rawIRValue;
+  }
+  if(millis()%10000 == 1) { 
+  if (client.connect(server,80)) { // "184.106.153.149" or api.thingspeak.com
+    String postStr = apiKey;
+    postStr +="&field1=";
+    postStr += String(sensor_data);
+    postStr += "\r\n\r\n";
+
+  
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+
+    Serial.print("heartrate : ");
+    Serial.print(sensor_data);
+    Serial.println("send to Thingspeak");
+  }
+  client.stop();
+
+  Serial.println("Waiting…");
+}
+  
+  // thingspeak needs minimum 15 sec delay between updates
+  //delay(10000);
+}
